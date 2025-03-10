a48a07850926ba36f31c9387a4c51ceb5247d695
Nishchal-ll <acharyanischal2004@gmail.com>
2025-03-10 22:09:57 +0545

    simpleprogram

---
+++ Simple Programs/largeamongthree.py
@@ -0,0 +1,11 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+if a>b and a>c :
+    print(a," is the largest")
+    
+if b>c and b>a:
+    print(b, "is the largest")
+    
+if c>a and c>b:
+    print(c," is the largest")
